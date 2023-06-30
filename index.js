c2c5bab639780537bc123ba154cf87dd2d2d7d7b
Hola-Code001 <Habioyeyusuph@gmail.com>
2023-07-01 00:52:14 +0100

    animation to close notifcation

--- index.js
+++ index.js
@@ -9,14 +9,13 @@ function createNotification() {
   const notification = document.createElement("div");
   container.appendChild(notification);
   notification.classList.add("active");
-  notification.innerHTML = "You have a new messages";
+  notification.innerHTML = `You have a new message`;
 
   setTimeout(() => {
     notification.classList.add("unactive");
-    console.log("removing");
-  }, 8000);
+  }, 5000);
 
   setTimeout(() => {
     notification.remove();
-  }, 10000);
+  }, 6000);
 }
